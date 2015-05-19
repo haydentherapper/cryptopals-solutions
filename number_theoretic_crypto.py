a14bbdd1b81bce400d664384c3fda57184e05d25
Hayden Blauzvern <hblauzvern@gmail.com>
2015-05-18 22:00:14 -0500

    Finished c36, neat SRP

--- number_theoretic_crypto.py
+++ number_theoretic_crypto.py
@@ -1,4 +1,6 @@
 import random
+from Crypto import Random
+from Crypto.Hash import SHA256
 
 class Alice:
     def __init__(self, g, p):
@@ -23,6 +25,78 @@ class Bob:
 
     def calc_key(self, A):
         self.s = modexp(base=A, exp=self.secret_b, mod=self.p)
+
+class Client:
+    def __init__(self, g, p, k, I, password):
+        self.g = g
+        self.p = p
+        self.k = k
+        self.I = I
+        self.password = password
+        self.secret_a = random.randint(0, p-1)
+
+    def calc_pub_A(self):
+        self.A = modexp(base=self.g, exp=self.secret_a, mod=self.p)
+
+    def receive_pub_B_salt(self, B, salt):
+        self.B = B
+        self.salt = salt
+
+    def calc_hash_A_B(self):
+        h = SHA256.new()
+        h.update(str(self.A).encode()+str(self.B).encode())
+        uH = h.hexdigest()
+        self.u = int(uH, 16)
+
+    def calc_shared_key(self):
+        h = SHA256.new()
+        h.update(self.salt+self.password)
+        xH = h.hexdigest()
+        x = int(xH, 16)
+        v = modexp(self.g, x, self.p)
+        S = modexp(self.B - (self.k*v), self.secret_a + (self.u*x), self.p)
+        h = SHA256.new()
+        h.update(str(S).encode())
+        self.K = h.digest()
+
+class Server:
+    def __init__(self, g, p, k, I, password):
+        self.g = g
+        self.p = p
+        self.k = k
+        self.I = I
+        self.password = password
+        self.secret_b = random.randint(0, p-1)
+
+    def gen_salt(self):
+        self.salt = Random.new().read(16)
+
+    def gen_pass_verifier(self):
+        h = SHA256.new()
+        h.update(self.salt+self.password)
+        xH = h.hexdigest() # Insecure hashed password
+        x = int(xH, 16)
+        self.v = modexp(self.g, x, self.p) # Password verifier
+
+    def calc_pub_B(self):
+        self.B = (self.k * self.v) + modexp(base=self.g, exp=self.secret_b, mod=self.p)
+
+    def receive_pub_A(self, A):
+        self.A = A
+
+    def calc_hash_A_B(self):
+        h = SHA256.new()
+        h.update(str(self.A).encode()+str(self.B).encode())
+        uH = h.hexdigest()
+        self.u = int(uH, 16)
+
+    def calc_shared_key(self):
+        v_u = modexp(self.v, self.u, self.p)
+        S = modexp(self.A * v_u, self.secret_b, self.p)
+        h = SHA256.new()
+        h.update(str(S).encode())
+        self.K = h.digest()
+
 
 # Fast Right-to-Left modular exponentiation
 # Runs in O(log(exp))
--- set5_test.py
+++ set5_test.py
@@ -1,6 +1,6 @@
 from number_theoretic_crypto import *
 from Crypto.Cipher import AES
-from Crypto.Hash import SHA 
+from Crypto.Hash import SHA, SHA256, HMAC
 from Crypto import Random
 
 def c33():
@@ -178,9 +178,49 @@ def c35():
     print("C35 passed!\n")
 
 def c36():
-    
+    print("1. Agree on shared values N, g, k, I (email) and P (password)")
+    g = 2
+    p = int(''.join(
+        """ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
+        e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
+        3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
+        6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
+        24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
+        c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
+        bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
+        fffffffffffff"""
+        .split()), 16)
+    k = 3
+    I = b'me'
+    password = b'yellow submarine'
+    client = Client(g, p, k, I, password)
+    server = Server(g, p, k, I, password)
+    print("2. Server generates salt, hashes salt|password, and creates password verifier")
+    server.gen_salt()
+    server.gen_pass_verifier()
+    print("3. Client sends username and public A")
+    client.calc_pub_A()
+    server.receive_pub_A(client.A)
+    print("4. Server sends salt and public B")
+    server.calc_pub_B()
+    client.receive_pub_B_salt(server.B, server.salt)
+    print("5. Both compute hash of A|B")
+    client.calc_hash_A_B()
+    server.calc_hash_A_B()
+    print("6. Client generates shared key")
+    client.calc_shared_key()
+    print("7. Server generates shared key")
+    server.calc_shared_key()
+
+    print("8. Authenticate by checking that both keys match")
+    client_h = HMAC.new(client.K, msg=client.salt, digestmod=SHA256)
+    server_h = HMAC.new(server.K, msg=server.salt, digestmod=SHA256)
+    assert(server.K == client.K)
+    assert(client_h.hexdigest() == server_h.hexdigest())
+    print("C36 passed!\n")
 
 if __name__ == '__main__':
     c33()
     c34()
     c35()
+    c36()
